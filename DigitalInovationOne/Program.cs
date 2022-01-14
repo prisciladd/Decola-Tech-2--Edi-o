38f8c3a3329dd85783cf011c8888b41d12009318
Priscila Da Dalt <priscila.dalt@aluno.faculdadeimpacta.com.br>
2022-01-13 22:24:41 -0300

    commit inicial c# e .net

---
+++ DigitalInovationOne/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DigitalInovationOne.exemplos;
+
+//exemplo 1
+
+int numeroDeVezes = 5;
+
+for (int i=0;i<numeroDeVezes;i++){
+    Console.WriteLine($"Bem vindo ao curso de .Net {i}");
+}
+
+//exemplo 2
+
+namespace DigitalInovationOne.exemplos
+{
+    class Program
+    {
+        static void Main()
+        {
+            var s = new Pilha();
+            s.Empilha(1);
+            s.Empilha(10);
+            s.Empilha(100);
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+        }
+    }
+}
